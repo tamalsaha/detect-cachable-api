049e9f9d8f362e8443ce47195d24e069b15273b4
Tamal Saha <tamal@appscode.com>
2022-01-12 10:51:24 -0800

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/discovery"
 	//	"time"
 
 	// 	"github.com/nats-io/nats.go"
@@ -101,4 +105,83 @@ func run() error {
 		fmt.Println(n.Name)
 	}
 	return nil
+}
+
+type Cachable interface {
+	GVK(gvk schema.GroupVersionKind) (bool, error)
+	GVR(gvr schema.GroupVersionResource) (bool, error)
+}
+
+type cachable struct {
+	resources map[string][]metav1.APIResource
+}
+
+var _ Cachable = cachable{}
+
+func NewCachable(cl discovery.DiscoveryInterface) (Cachable, error) {
+	_, list, err := cl.ServerGroupsAndResources()
+	if !discovery.IsGroupDiscoveryFailedError(err) {
+		return nil, err
+	}
+	m := map[string][]metav1.APIResource{}
+	for _, rl := range list {
+		m[rl.GroupVersion] = rl.APIResources
+	}
+	return cachable{resources: m}, nil
+}
+
+func (c cachable) GVK(gvk schema.GroupVersionKind) (bool, error) {
+	rl, ok := c.resources[gvk.GroupVersion().String()]
+	if !ok {
+		return false, &meta.NoKindMatchError{
+			GroupKind:        gvk.GroupKind(),
+			SearchedVersions: []string{gvk.Version},
+		}
+	}
+	for _, r := range rl {
+		if r.Kind != gvk.Kind {
+			continue
+		}
+		var canList, canWatch bool
+		for _, verb := range r.Verbs {
+			if verb == "list" {
+				canList = true
+			}
+			if verb == "watch" {
+				canWatch = true
+			}
+		}
+		return canList && canWatch, nil
+	}
+	return false, &meta.NoKindMatchError{
+		GroupKind:        gvk.GroupKind(),
+		SearchedVersions: []string{gvk.Version},
+	}
+}
+
+func (c cachable) GVR(gvr schema.GroupVersionResource) (bool, error) {
+	rl, ok := c.resources[gvr.GroupVersion().String()]
+	if !ok {
+		return false, &meta.NoResourceMatchError{
+			PartialResource: gvr,
+		}
+	}
+	for _, r := range rl {
+		if r.Name != gvr.Resource {
+			continue
+		}
+		var canList, canWatch bool
+		for _, verb := range r.Verbs {
+			if verb == "list" {
+				canList = true
+			}
+			if verb == "watch" {
+				canWatch = true
+			}
+		}
+		return canList && canWatch, nil
+	}
+	return false, &meta.NoResourceMatchError{
+		PartialResource: gvr,
+	}
 }
